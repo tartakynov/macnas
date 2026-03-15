80b120199e7bcb49b7579f9893a4ec1bf63b784c
Artem Tartakynov <artem.tad@gmail.com>
2026-03-15 16:41:40 -0400

    update release sha

--- Casks/macnas.rb
+++ Casks/macnas.rb
@@ -1,6 +1,6 @@
 cask "macnas" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "d2708bcc5ca63b537cc0d4187e372a5f1a58fa04a62db0b1d32d2ccb0150b767"
 
   url "https://github.com/tartakynov/macnas/releases/download/v#{version}/MacNAS-#{version}.zip"
   name "MacNAS"
